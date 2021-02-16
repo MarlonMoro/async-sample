09509e72d141c6fc76a9712e730f0d660efe1b95
Marlon Moro <marlon@ciclic.com.br>
2021-02-15 21:41:57 -0300

    feat: Initial commit

---
+++ src/main/java/br/com/marlonmoro/asyncsample/AsyncSampleApplication.java
@@ -0,0 +1,15 @@
+package br.com.marlonmoro.asyncsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSampleApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/br/com/marlonmoro/asyncsample/AsyncSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonmoro.asyncsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
