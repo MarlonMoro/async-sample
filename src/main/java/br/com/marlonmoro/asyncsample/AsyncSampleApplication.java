77582298e079209a677a74256da61fd86cb9c769
Marlon Moro <marlon@ciclic.com.br>
2021-02-15 22:24:12 -0300

    feat: added Executor config

--- src/main/java/br/com/marlonmoro/asyncsample/AsyncSampleApplication.java
+++ src/main/java/br/com/marlonmoro/asyncsample/AsyncSampleApplication.java
@@ -1,8 +1,11 @@
 package br.com.marlonmoro.asyncsample;
 
+import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -10,6 +13,16 @@ public class AsyncSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncSampleApplication.class, args);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(3);
+		executor.setQueueCapacity(500);
+		executor.initialize();
+		return executor;
 	}
 
 
